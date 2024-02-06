a4442b56ab795026fc17fa3af7942a92962f2393
Ao Wang <wangaotongji@gmail.com>
2024-02-06 19:09:57 +0800

    Update index.jsx
    轮播效果

--- app/(home)/EcosystemEc3/index.jsx
+++ app/(home)/EcosystemEc3/index.jsx
@@ -22,6 +22,16 @@ const EcosystemEc3 = ({className = 'ecosystem-wrapper'}) => {
                                 <p className="carousel-text">Uhome focuses on manufacturing lithium-ion batteries for residential and small-scale energy storage systems, offering adaptable solutions for independent micro-grids by integrating energy storage and management systems.</p>
                             </div>
                         </div>
+                        <div className="carousel-item">
+                            <div className="carousel-logo">
+                                <Image className="uhome-img" width={160} height={186} src="/images/ecosystem/uhome.png" alt="UHome" />
+                                <Image className="uhome-text" width={160} height={40} src="/images/ecosystem/uhome-text.png" alt="UHome" />
+                            </div>
+                            <div className="carousel-content">
+                                <Image className="quote-img" width={40} height={36} src="/images/carousel/quote.svg" alt="quote" />
+                                <p className="carousel-text">Uhome focuses on manufacturing lithium-ion batteries for residential and small-scale energy storage systems, offering adaptable solutions for independent micro-grids by integrating energy storage and management systems.</p>
+                            </div>
+                        </div>
                     </Carousel>
                 </div>
             </div>
