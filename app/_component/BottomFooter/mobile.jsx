59ea199eb09f23c106145fbcd53ead9df165054d
snowome <32677138+snowome@users.noreply.github.com>
2024-02-18 17:11:26 +0800

    '优化手机端邮件'

--- app/_component/BottomFooter/mobile.jsx
+++ app/_component/BottomFooter/mobile.jsx
@@ -5,44 +5,46 @@ import {Button, Collapse, Modal} from 'antd-mobile';
 import './mobile.scss';
 import useScrollAnimate from '@/_hooks/useScrollAnimate.js'
 import allLinks from "./allLinks.js";
+import {linkLocation} from "@/config";
 
 const BottomFooter = ({className = ''}) => {
-    const [email, setEmail] = useState('');
+    const [emailBody, setEmailBody] = useState('');
     const emailInputRef = useRef(null);
     const scrollRef = useScrollAnimate({offset: 200});
 
     useEffect(() => {
         const _current = emailInputRef.current;
         if (_current) {
-            if (email.trim() === '') {
-                _current.setCustomValidity('Enter your email address .');
-            } else if (!(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email))) {
-                _current.setCustomValidity('The email address is illegal .');
+            if (emailBody.trim() === '') {
+                _current.setCustomValidity('Enter your email body .');
+            // } else if (!(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email))) {
+            //     _current.setCustomValidity('The email address is illegal .');
             } else {
                 _current.setCustomValidity('');
             }
         }
-    }, [email]);
+    }, [emailBody]);
 
     return (
         <footer className={`bottom-footer ${className}`}>
             <div className="inner">
                 <div className="contact-us" ref={scrollRef}>
-                    <form className="mail-form" action={`mailto:${email}`} method="post" encType="text/plain">
+                    <div className="mail-form">
                         <label className="text animate__animated animate__slower" data-animate="animate__flipInY">Get in touch with us</label>
                         <div className="content animate__animated" data-animate="animate__fadeInLeft">
                             <div className="email-field">
-                                <input type="email" id="from" name="from" required
-                                       ref={emailInputRef}
-                                       value={email}
-                                       onChange={e => setEmail(e.target.value)}
-                                       placeholder="Enter your email address" autoComplete="off"/>
+                                <input
+                                    type="text"
+                                    required
+                                    ref={emailInputRef}
+                                    value={emailBody}
+                                    onChange={e => setEmailBody(e.target.value)}
+                                    placeholder="Enter your email body" autoComplete="off"/>
                             </div>
-                            {/*<input type="hidden" id="subject" name="subject" value="Customer letters from Ec3 website"/>*/}
-                            {/*<input type="hidden" id="body" name="body" value="" />*/}
-                            <Button type="submit" color="success" fill="solid">Send Email</Button>
+                            <a className="adm-button adm-button-success adm-button-shape-default"
+                               href={`${linkLocation['support@Ec-3.io']}?subject=Customer letters from Ec3 website&body=${emailBody}`}>Send Email</a>
                         </div>
-                    </form>
+                    </div>
                 </div>
                 <Collapse accordion>
                     {allLinks.map(({title, links}, key) => (
