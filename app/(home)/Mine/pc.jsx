6bc1c34121e54a6f1758c6478c713b67ed849adb
snowome <32677138+snowome@users.noreply.github.com>
2024-02-24 05:57:13 +0800

    'PC端增加预售倒计时按钮'

--- app/(home)/Mine/pc.jsx
+++ app/(home)/Mine/pc.jsx
@@ -1,11 +1,34 @@
+import React, { useState, useMemo, useEffect } from 'react';
 import Image from 'next/image';
+import {Button} from 'antd';
 import useScrollAnimate from '@/_hooks/useScrollAnimate.js'
 import avatar1 from '~/images/mine/avatar-1.svg';
 import avatar2 from '~/images/mine/avatar-2.svg';
 import energyStorageImg from '~/images/mine/energy-storage.svg';
 
+const START_TIME = new Date('2024-03-01 00:00:00').getTime();
+const duration = parseInt((START_TIME - Date.now()) / 1000);
 const MinePC = ({className = 'mine'}) => {
+    // 倒计时：秒
+    const [time, setTime] = useState(duration);
     const scrollRef = useScrollAnimate({imgHalfHeight: 200});
+
+    // 根据倒计时的秒，显示可视化的日期
+    const formatTime = useMemo(() => {
+        const days = (Math.floor(time / (60 * 60 * 24)) + '').padStart(2, '0');
+        const hours = (Math.floor((time % (60 * 60 * 24)) / (60 * 60)) + '').padStart(2, '0');
+        const minutes = (Math.floor((time % (60 * 60)) / 60) + '').padStart(2, '0');
+        const seconds = (time % 60 + '').padStart(2, '0');
+        return `${days}d : ${hours}h : ${minutes}m : ${seconds}s`;
+    }, [time]);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(curTime => curTime - 1);
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
 
     return (
         <div className={className}>
@@ -36,13 +59,18 @@ const MinePC = ({className = 'mine'}) => {
                             </div>
                         </div>
                     </div>
-                    <div className="image-wrapper animate__animated" data-animate="animate__fadeInRight">
-                        <Image src={energyStorageImg} alt="Energy storage" />
+                    <div className={`image-wrapper ${time > 0 ? 'pre-sale' : ''}`}>
+                        <Image className="animate__animated" data-animate="animate__fadeInRight"
+                               src={energyStorageImg} alt="Energy storage" />
+                        <Button type="primary"
+                            // href="http://www.baidu.com"
+                                target="_blank">Pre-Sale : <label>{formatTime}</label></Button>
                     </div>
                 </div>
             </div>
         </div>
     );
 };
+
 
 export default MinePC;
