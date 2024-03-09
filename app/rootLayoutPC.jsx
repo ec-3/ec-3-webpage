72caeccb1917c36455fa352b694a9262353d5481
snowome <32677138+snowome@users.noreply.github.com>
2024-03-10 03:20:26 +0800

    test7

--- app/rootLayoutPC.jsx
+++ app/rootLayoutPC.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { ConfigProvider } from 'antd';
 import theme from '@/_theme/blueTheme.js';
-import { AntdRegistry } from '@ant-design/nextjs-registry';
+// import { AntdRegistry } from '@ant-design/nextjs-registry';
 import "./_scss/index.scss";
 import HeadEc3 from '@/_component/HeadEc3';
 import { Analytics } from '@vercel/analytics/react';
@@ -11,7 +11,8 @@ export default function RootLayoutPC({children}) {
         <html lang="en">
         <HeadEc3 isMobile={false} />
         {/*<ConfigProvider theme={theme}>*/}
-            <body><AntdRegistry>{children}<Analytics /></AntdRegistry></body>
+        {/*    <body><AntdRegistry>{children}<Analytics /></AntdRegistry></body>*/}
+            <body>{children}<Analytics /></body>
         {/*</ConfigProvider>*/}
         </html>
     );
